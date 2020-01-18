fc19ba034fa88e8ed9b955f1b082367949d29bc8
cbfbl <cbfbl651@gmail.com>
2020-01-18 17:01:47 +0200

    deleted extra text

--- frontend/src/Components/Notification.js
+++ frontend/src/Components/Notification.js
@@ -2,52 +2,6 @@ import React, { Component } from 'react';
 import { flask_server_adress } from '../utils';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
-import { withRouter } from 'react-router-dom';
-
-class Alert extends Component {
-	constructor() {
-		super();
-		this.removeAlert = this.removeAlert.bind(this);
-		this.state = {
-			post_id: ''
-		};
-	}
-	componentDidMount() {
-		this.setState({
-			post_id: this.props.post_id
-		});
-	}
-	render() {
-		return (
-			<div>
-				post with id :{this.state.post_id} was edited
-				<input type="submit" onClick={this.removeAlert} value="Clear notification" />
-			</div>
-		);
-	}
-	removeAlert() {
-		const user_token = localStorage.usertoken;
-		let decoded = '';
-		if (user_token) {
-			decoded = jwt_decode(user_token);
-			const logged_user_id = decoded.identity.id;
-			axios.defaults.withCredentials = true;
-			axios
-				.post(flask_server_adress + '/notification/delete', {
-					user_id: logged_user_id,
-					post_id: this.state.post_id
-				})
-				.then((response) => {
-					if (response.data === 'deleted') {
-						this.props.history.push('/');
-					}
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	}
-}
 
 class Notification extends Component {
 	constructor() {
